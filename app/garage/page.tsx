fef50fa0231684165e36a71879c0bbfe02a642d5
Viljar <viljar.hoem.olsen@gmail.com>
2025-03-03 18:41:42 +0100

    Improved landing page UI

--- app/garage/page.tsx
+++ app/garage/page.tsx
@@ -1,5 +1,4 @@
 import { GarageLayout } from "@/components/garage-layout";
-import { Navbar } from "@/components/navbar";
 
 /**
  * Garage Page:
@@ -10,8 +9,6 @@ import { Navbar } from "@/components/navbar";
 export default function GaragePage() {
   return (
     <div className="min-h-screen flex flex-col bg-gray-100">
-      <Navbar />
-
       <div className="flex justify-center items-center flex-grow p-4">
         <div
           className="w-full max-w-6xl bg-white shadow-lg rounded-lg
