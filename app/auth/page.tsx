f5ed0238563dec51a33f7f3154c94d08aa2ec80a
Viljar Hoem-Olsen <viljar.hoem.olsen@gmail.com>
2025-04-07 09:39:54 +0200

    BAC-58: Removed forgot passowrd button. It had no functionality

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -255,12 +255,6 @@ export default function AuthPage() {
                       <Label htmlFor="password" className="text-sm font-medium">
                         Password
                       </Label>
-                      <Link
-                        href="#"
-                        className="text-xs text-gray-500 hover:text-gray-800"
-                      >
-                        Forgot password?
-                      </Link>
                     </div>
                     <div className="relative">
                       <Lock className="absolute left-3 top-2.5 h-5 w-5 text-gray-400" />
